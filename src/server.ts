75d9a08a5252fcb8e1c1686101dfdc6e51578ef4
jamilWebdesign2024 <jamiluddinjishan@gmail.com>
2025-12-09 01:14:52 +0600

    added

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import {Pool} from "pg";
 import dotenv from "dotenv"
 import path from "path"
@@ -100,6 +100,12 @@ const initDB = async () => {
   // console.log("'bookings' table created (if not exists).");
 };
 
+// logger middleware
+const logger = (req:Request, res:Response, next:NextFunction)=>{
+ console.log(`[${new Date().toISOString()}] ${req.method}${req.path}\n`);
+   next()
+}
+
 initDB();
 
 
@@ -110,18 +116,158 @@ app.use(express.json());
 
 
 
-app.get('/', (req:Request, res:Response) => {
+app.get('/', logger, (req:Request, res:Response) => {
   res.send('Hello Next Level Developers')
 })
 
-app.post('/', (req:Request, res:Response) => {
-  console.log(req.body);
+// users CRUD
+app.post('/users', async(req:Request, res:Response) => {
+  const {name, email, password, phone, role} = req.body;
+
+  try{
+    const result = await pool.query(`INSERT INTO users(name, email, password, phone, role) VALUES($1, $2, $3, $4, $5) RETURNING *`, [name, email, password, phone, role]);
+    
+    // console.log(result.rows[0]);
+    res.status(201).json({
+      success: false,
+      message: "Data Inserted successfully",
+      data: result.rows[0]
+    })
+   
+  }
   
-  res.status(201).json({
-    success: true,
-    message: "API is working"
+  catch(err:any){
+    res.status(500).json({
+      success: false,
+      message: err.message
+    })
+  }
+
+})
+
+
+// localhost:5000/users get
+app.get("/users", async(req:Request, res:Response)=>{
+    try{
+      const result = await pool.query(`SELECT * FROM users`);
+
+      res.status(200).json({
+        success: true,
+        message: "Users retrieved successfully",
+        data: result.rows
+      })
+    }
+    catch(err:any){
+      res.status(500).json({
+        success: false,
+        message: err.message,
+        details: err
+      })
+    }
+})
+
+// localhost:5000/users/:id get
+app.get("/users/:id", async(req:Request, res:Response)=>{
+  console.log(req.params.id);
+  
+  try{
+    const result = await pool.query(`SELECT * FROM users WHERE id = $1`, [req.params.id]);
+
+    if(result.rows.length === 0){
+      res.status(404).json({
+        success: false,
+        message: "User not found",
+      })
+    }
+    else{
+      res.status(200).json({
+        success:true,
+        message: "User fetched successfully",
+        data: result.rows[0]
+      })
+    }
+    
+  }catch(err:any){
+    res.status(500).json({
+        success: false,
+        message: err.message,
+      })
+  }
+})
+
+// localhost:5000/users/:id put
+app.put("/users/:id", async(req:Request, res:Response)=>{
+  // console.log(req.params.id);
+ const {name, email, password, phone, role} = req.body;
+
+  try{
+    const result = await pool.query(`UPDATE users SET name=$1, email=$2, password=$3, phone=$4, role=$5 WHERE id=$6 RETURNING *`, [name, email, password, phone, role, req.params.id]);
+
+    if(result.rows.length === 0){
+      res.status(404).json({
+        success: false,
+        message: "User not found",
+      })
+    }
+    else{
+      res.status(200).json({
+        success:true,
+        message: "User updated successfully",
+        data: result.rows[0]
+      })
+    }
+    
+  }catch(err:any){
+    res.status(500).json({
+        success: false,
+        message: err.message,
+      })
+  }
+});
+
+
+// localhost:5000/users/:id DELETE
+app.delete("/users/:id", async(req:Request, res:Response)=>{
+  // console.log(req.params.id);
+  
+  try{
+    const result = await pool.query(`DELETE FROM users WHERE id = $1`, [req.params.id]);
+
+    console.log(result);
+    
+
+    if(result.rowCount === 0){
+      res.status(404).json({
+        success: false,
+        message: "User not found",
+      })
+    }
+    else{
+      res.status(200).json({
+        success:true,
+        message: "User deleted successfully",
+        data: result.rows
+      })
+    }
+    
+  }catch(err:any){
+    res.status(500).json({
+        success: false,
+        message: err.message,
+      })
+  }
+})
+
+
+app.use((req: Request, res:Response)=>{
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+    path: req.path,
   })
 })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
